240275d2260e33e2b0eb81d815b700a7b26303be
Nguyen Thi Hai Anh <haianhnt.st@gmail.com>
2018-06-10 20:36:00 +0700

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require("assert");
+const dboper = require("./operations");
 
 const url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, (err, client) => {
     assert.equal(err, null);
+    const db = client.db("conFusion");
 
     console.log("Connected correctly to server");
-    const db = client.db("conFusion");
+    dboper.insertDocument(db, {name:"Vadonut", description:"test"}, 
+        "dishes", (result) => {
+        console.log("Insert Document: \n", result.ops);
+
+        dboper.findDocuments(db, "dishes", (docs) => {
+            console.log("Found Documents: \n", docs);
+
+            dboper.updateDocument(db, {name:"Vadonut"}, 
+                {description: "Update Test"}, "dishes", 
+                (result) => {
+                console.log("Updated Document: \n", result.result);
+
+                dboper.findDocuments(db, "dishes", (docs) => {
+                    console.log("Found Updated Documents: \n", docs);
+
+                    db.dropCollection("dishes", (result) => {
+                        console.log("Dropped Collection: ", result);
+
+                        client.close();
+                    
+                    });
+                });
+            });
+        });
+    });
+});
+
+/*
+const db = client.db("conFusion");
     const collection = db.collection("dishes");
     collection.insertOne({"name":"Nguyen", "description":"test"},
 
@@ -29,4 +59,4 @@ MongoClient.connect(url, (err, client) => {
             });
         });
     });
-});
+*/
