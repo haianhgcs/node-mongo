35e6a59d48d29fba4b6f6d5b7dd40dfab977c855
Nguyen Thi Hai Anh <haianhnt.st@gmail.com>
2018-06-10 23:18:52 +0700

    Node Callback Hell and Promises

--- index.js
+++ index.js
@@ -4,59 +4,35 @@ const dboper = require("./operations");
 
 const url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, (err, client) => {
-    assert.equal(err, null);
+MongoClient
+.connect(url)
+.then((client) => {
     const db = client.db("conFusion");
-
     console.log("Connected correctly to server");
-    dboper.insertDocument(db, {name:"Vadonut", description:"test"}, 
-        "dishes", (result) => {
-        console.log("Insert Document: \n", result.ops);
 
-        dboper.findDocuments(db, "dishes", (docs) => {
-            console.log("Found Documents: \n", docs);
+    dboper.insertDocument(db, {name:"Vadonut", description:"test"}, "dishes")
+          .then((result) => {
+                console.log("Insert Document: \n", result.ops);
+                return dboper.findDocuments(db, "dishes");
+          })
+          .then((docs) => {
+                console.log("Found Documents: \n", docs);
 
-            dboper.updateDocument(db, {name:"Vadonut"}, 
-                {description: "Update Test"}, "dishes", 
-                (result) => {
+                return dboper.updateDocument(db, {name:"Vadonut"}, {description: "Update Test"}, "dishes");
+          })
+          .then((result) => {
                 console.log("Updated Document: \n", result.result);
 
-                dboper.findDocuments(db, "dishes", (docs) => {
-                    console.log("Found Updated Documents: \n", docs);
-
-                    db.dropCollection("dishes", (result) => {
-                        console.log("Dropped Collection: ", result);
-
-                        client.close();
-                    
-                    });
-                });
-            });
-        });
-    });
-});
-
-/*
-const db = client.db("conFusion");
-    const collection = db.collection("dishes");
-    collection.insertOne({"name":"Nguyen", "description":"test"},
-
-    (err, result) => {
-        assert.equal(err, null);
-
-        console.log("After insert: ");
-        console.log(result.ops);
-
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null);
-
-            console.log("Found: \n");
-            console.log(docs);
-
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err, null);
+                return dboper.findDocuments(db, "dishes");
+          })
+          .then((docs) => {
+                console.log("Found Updated Documents: \n", docs);
+                return db.dropCollection("dishes");
+          })
+          .then((result) => {
+                console.log("Dropped Collection: ", result);
                 client.close();
-            });
-        });
-    });
-*/
+           })
+           .catch((err) => console.log(err));
+}, (err) => console.log(err))
+.catch((err) => console.log(err));
